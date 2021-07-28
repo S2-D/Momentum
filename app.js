5dd5155c5a9d116a06debd8048d355b5a245cc23
Sodam Shin <damsoshin@gmail.com>
2021-07-28 20:04:48 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CLASS = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASS)
   const username = loginInput.value;
-  console.log(username)
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS)
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
+
+
+
+// 
+function handleLinkClick(event){
+  event.preventDefault();
+  console.dir(event)
+  alert('clicked')
+}
