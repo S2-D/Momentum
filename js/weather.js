85c153c9e67685dda0bb8f6360862dda8f727b97
Sodam Shin <damsoshin@gmail.com>
2021-08-08 23:38:10 +0900

    weather icon 추가

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,7 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
+const weatherDiv = document.getElementById("weather");
+const city = document.querySelector("#weather span:first-child");
+
+const weather = document.querySelector("#weather span:last-child");
 
 const API_KEY = "4725f2232e942b4710dd0676bb0c2497";
 
@@ -11,7 +13,11 @@ function onGeoOk(position) {
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C `;
+      const wether_icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      const icon_image = document.createElement("IMG");
+      icon_image.src = wether_icon;
+      weatherDiv.appendChild(icon_image);
     });
 }
 function onGeoError() {
